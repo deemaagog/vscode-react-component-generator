14414571577620dfe99f9925d6a7ff1fea1d6eb3
deemaagog <deemaagog@gmail.com>
2019-11-23 13:12:31 +0300

    update templates

---
+++ classComponent_template/__ComponentName__.less.d.ts
@@ -0,0 +1,4 @@
+export interface I__ComponentName__Less {}
+
+export const locals: I__ComponentName__Less;
+export default locals;
---
+++ classComponent_template/__ComponentName__.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export interface __ComponentName__Props {}
+
+const __ComponentName__: React.FunctionComponent<__ComponentName__Props> = () => (
+  <>
+    <h1>__ComponentName__ Component</h1>
+  </>
+); 
+
+export default __ComponentName__;
